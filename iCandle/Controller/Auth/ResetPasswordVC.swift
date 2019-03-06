4924b51d73b2a2d67ad1963d13d0cfe112a10337
Eashan Ashrafulzaman <eashan.ashrafulzaman@live.com>
2019-03-06 19:58:45 +0600

    Change password design done.

---
+++ iCandle/Controller/Auth/ResetPasswordVC.swift
@@ -0,0 +1,17 @@
+//
+//  ResetPasswordVC.swift
+//  iCandle
+//
+//  Created by Parves Kawser on 6/3/19.
+//  Copyright © 2019 iCandle. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
